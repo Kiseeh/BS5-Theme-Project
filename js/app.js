e1ed08123aa7b75c7d5d39cab5a341b8b2206314
Patricia Nkirote <PNkirote@izone.local>
2023-06-05 17:21:49 +0300

    added services section

--- js/app.js
+++ js/app.js
@@ -10,13 +10,32 @@ $(document).ready(function(){
         navText:['PREV', 'NEXT'],
         responsive:{
             0:{
-                
+                nav: false,
             },
-            600:{
-                
+            768: {
+                nav: true,
+            }
+        }
+    })
+});
+
+// PROJECT SLIDER
+$(document).ready(function(){
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:false,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                nav: false,
             },
-            1000:{
-                
+            768: {
+                nav: true,
+            },
+            1148: {
+
             }
         }
     })
